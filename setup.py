b09d7ee594de11245d7a62f23d8edef74287f977
zijunzhao <zz2563@columbia.edu>
2019-09-07 11:27:19 -0400

    add pkg joblib in setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='neuro_morpho_toolbox',
       install_requires=['numpy', 'sklearn', 'scipy', 'pandas', 'SimpleITK',
                         'matplotlib', 'python-igraph', 'seaborn','sparse',
 			'hdbscan','colorlover', 'plotly', 'retrying',
-			'ipywidgets','umap-learn', 'Cython'
+			'ipywidgets','umap-learn', 'Cython','joblib'
                         ],#,'chart-studio'
       test_suite='nose.collector',
       tests_require=['nose', 'nose-cover3']
