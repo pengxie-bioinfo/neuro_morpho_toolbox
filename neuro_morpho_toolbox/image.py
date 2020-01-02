a91cf29e92889ee8849fd68ac99b709ce9010259
zijun-zhao <zz2563@columbia.edu>
2020-01-02 23:03:30 +0800

    modified annotation for func writeNRRD

--- neuro_morpho_toolbox/image.py
+++ neuro_morpho_toolbox/image.py
@@ -3,10 +3,13 @@ import numpy as np
 import pickle
 import sparse
 import os
+
+
 class image:
     '''
     This class is originally defined to load CCF atlas (value of a pixel is the ID of a brain structure)
     '''
+
     def __init__(self, file=None, pickle_file=None):
         assert ((file is not None) | (pickle_file is not None)), "Error in initializing image object."
         if pickle_file is None:
@@ -17,14 +20,15 @@ class image:
             To be deprecated: Save large image files as numpy array takes to much space
             '''
             self.file = "Given image"
-            array, spacing = pickle.load(open(pickle_file, 'rb')) # Note that pickle does not support SimpleITK image class
+            array, spacing = pickle.load(
+                open(pickle_file, 'rb'))  # Note that pickle does not support SimpleITK image class
             if type(array) == sparse.coo.core.COO:
                 array = array.todense()
             array = np.swapaxes(array, 0, 2)
             self.image = sitk.GetImageFromArray(array)
             self.image.SetSpacing(spacing)
         self.array = sitk.GetArrayViewFromImage(self.image)
-        self.array = np.swapaxes(self.array, 0, 2) # Swap XZ axis to meet the convention of V3D
+        self.array = np.swapaxes(self.array, 0, 2)  # Swap XZ axis to meet the convention of V3D
         # self.values = np.unique(self.array).tolist()
         self.axes = ["x", "y", "z"]
         self.size = dict(zip(self.axes, self.image.GetSize()))
@@ -35,33 +39,29 @@ class image:
                                      self.size["z"] * self.space["z"],
                                      ]
                                     ))
-    
 
-    def writeNRRD(input_M, path, IDlist = [1]):
+    def writeNRRD(input_M, path, IDlist=[1]):
         '''
         Write interested region in a 3D array to nrrd file, with interested region to be 1
         :param input_M: an 3D array, the value at each point is the ID of specific region
         :param IDlist: a list containing interested value
         :param path: the destination to store the output
-        :param range_y:
-        :param flipF: whether illustrating half part of the result or to illustrate the full result.
-                        It does not influce the saving result.
         :return: No
         '''
-        assert type(IDlist) ==list, "Please using list to input the interested regionID"
+        assert type(IDlist) == list, "Please using list to input the interested regionID"
         folder = os.path.exists(path)
-        if not folder:         
-            os.makedirs(path)  
+        if not folder:
+            os.makedirs(path)
         data = np.zeros(input_M.shape)
         for iterID in IDlist:
-            data[np.where(input_M==iterID)]=1
+            data[np.where(input_M == iterID)] = 1
         data = data.astype(np.int16)
-        filename = str(path)+ '/'
+        filename = str(path) + '/'
         for iterID in IDlist:
             filename = filename + str(iterID)
             filename = filename + '_'
-        filename = filename +'.nrrd'
+        filename = filename + '.nrrd'
         itkimage = sitk.GetImageFromArray(data, isVector=False)
-        sitk.WriteImage(itkimage, filename, True) 
-        
+        sitk.WriteImage(itkimage, filename, True)
+
 
